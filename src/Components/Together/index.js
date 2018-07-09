665812ad32a3406d054eded328ce1bdc689463c6
João Araújo <juauzaraujo@gmail.com>
2018-07-09 16:02:49 +0100

    add title

---
+++ src/Components/Together/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './index.css';
+import { styles } from '../../styles.js';
+
+export default class Together extends Component {
+    render() {
+        return <div className="Together">
+            <h1 style={styles.titleStyles.titleH1}>Let's work together</h1>
+        </div>
+    }
+}
